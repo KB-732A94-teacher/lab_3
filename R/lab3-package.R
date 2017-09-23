67a362b776950fae3c787145f5179ab9c73c5d0d
Raikao <jandro1994@gmail.com>
2017-09-23 18:42:59 +0200

    package documentation

---
+++ R/lab3-package.R
@@ -0,0 +1,11 @@
+#' lab3: A package for calculating dijkstra and euclidean algorithms
+#'
+#' The lab3 package provides two important functions:
+#' dijkstra, euclidean
+#' 
+#' @section lab3 functions:
+#' dijkstra, euclidean
+#'
+#' @docType package
+#' @name lab3-package
+NULL
